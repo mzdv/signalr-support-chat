cca27be847a3d800f3b87af7c516cb00e204e66b
Milos Zivadinovic <mzdv@outlook.com>
2015-08-06 01:14:42 +0200

    Implemented the Users model. Thing that isn't implemented is the disconnection event; stubs (they are more stumps than stubs) have been placed and await debugging. Also, happy birthday to me.

--- ChatApp/MainHub.cs
+++ ChatApp/MainHub.cs
@@ -3,19 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Microsoft.AspNet.SignalR;
+using ChatApp.Model;
 
 namespace ChatApp
 {
     public class MainHub : Hub
     {
-        public void Hello()
+        public static List<User> users = new List<User>();
+
+        public void CreateUser(string name)
         {
-            Clients.All.hello();
+            User user = new User();
+            user.Id = 0;
+            user.Name = name;
+            user.AccountType = AccTypeEnum.DOCTOR;
+            user.Active = true;
+            user.Created_at = DateTime.Now;
+            user.Last_accessed = DateTime.Now;
+
+            users.Add(user);
+
+            Clients.All.userCreationCallback(user);                         
+        }
+
+        public void ListUsers()
+        {
+            Clients.All.generateActiveUsers(users);
         }
 
         public void Send(string name, string message)
         {
             Clients.All.broadcastMessage(name, message);
+        }
+
+        public void UserDisconnected(string name)   // stub for disconnection event
+        {
+            users.Remove(users.Find(x => x.Name == name));
+            // TO-DO: Database statement that will update the timestamps and the active status
+
+            Clients.All.generateActiveUsers(users);
         }
     }
 }
